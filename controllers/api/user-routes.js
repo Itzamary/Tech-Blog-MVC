0fb8007ffc201759a3c8981b10be5d029ad3e0e9
Itzamary Jimenez <itzamary1gonzalez@hotmail.com>
2022-05-30 18:28:29 -0500

    added the routes for the user and exported the routerwhere i connected it to other files using middleware and also exported those files router and used the router to the file in the server.js file. with middlware so we can now reach the database using sequelize.

---
+++ controllers/api/user-routes.js
@@ -0,0 +1,91 @@
+const router = require('express').Router();
+const {User} = require('../../models');
+
+//Get /api/users
+router.get('/', (req, res) =>{
+    User.findAll({
+        attributes: {exclude: ['password']}
+    })
+    .then(dbUserData => res.json(dbUserData))
+    .catch(err => {
+        console.log(err, 'error');
+        res.status(500).json(err);
+    });
+});
+
+// Get One /api/users/:id
+router.get('/:id', (req, res) => {
+    User.findOne({
+        attributes: {exclude: ['password']},
+        where: {
+            id: req.params.id
+        }
+    })
+    then(dbUserData => {
+        if (!dbUserData) {
+            res.status(404).json({message: 'No user found with this id'});
+            return;
+        }
+        res.json(dbUserData);
+    })
+    .catch(err => {
+        console.log(err, 'error2');
+        res.status(500).json(err);
+    });
+});
+
+// Post /api/users
+router.post('/', (req, res) => {
+    User.create({
+        username: req.body.username,
+        email: req.body.email,
+        password: req.body.password
+    })
+    .then(dbUserData => res.json(dbUserData))
+    .catch(err => {
+        console.log(err, 'error3');
+        res.status(500).json(err);
+    });
+});
+
+//Put /api/user/:id
+router.put('/:id', (req, res) => {
+    User.update(req.body, {
+        where: {
+            id: req.params.id
+        }
+    })
+    .then(dbUserData => {
+        if (!dbUserData[0]) {
+            res.status(404).json({message: 'No user found with this id'});
+            return;
+        }
+        res.json(dbUserData);
+    })
+    .catch(err => {
+        console.log(err, 'error4');
+        res.status(500).json(err);
+    });
+});
+
+//Delete /api/user/:id
+router.delete('/:id', (req, res) => {
+    User.destroy({
+        where: {
+            id: req.params.id
+        }
+    })
+    .then(dbUserData => {
+        if (!dbUserData) {
+            res.status(404).json({message: 'No user found with this id'});
+            return;
+        }
+        res.json(dbUserData);
+    })
+    .catch(err => {
+        console.log(err, 'error5');
+        res.status(500).json(err);
+    });
+});
+
+module.exports = router;
