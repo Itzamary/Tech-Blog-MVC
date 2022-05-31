0966e87b110cf1f459bd50191a538b00b1a8c12a
Itzamary Jimenez <itzamary1gonzalez@hotmail.com>
2022-05-30 22:04:21 -0500

    changed the name of the column in the post module

--- models/Post.js
+++ models/Post.js
@@ -15,11 +15,11 @@ Post.init(
             type: DataTypes.STRING,
             allowNull: false
         },
-        post_url: {
+        post_text: {
             type: DataTypes.STRING,
-            allowNull: false, 
+            allowNull: false,
             validate: {
-                isUrl: true
+                len: [1]
             }
         },
         user_id: {
