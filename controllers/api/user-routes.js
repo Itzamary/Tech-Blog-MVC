d449c3553d5c45cec57882a793a08b3c868e2b9d
Itzamary Jimenez <itzamary1gonzalez@hotmail.com>
2022-06-01 17:38:37 -0500

    created the sessions on routes in the user-routes.js file and the home-routes.js file.

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -41,7 +41,15 @@ router.post('/', (req, res) => {
         email: req.body.email,
         password: req.body.password
     })
-    .then(dbUserData => res.json(dbUserData))
+    .then(dbUserData => {
+        req.session.save(() => {
+            req.session.user_id = dbUserData.id;
+            req.session.username = dbUserData.username;
+            req.session.loggedIn = true;
+
+            res.json(dbUserData);
+        })
+    })
     .catch(err => {
         console.log(err, 'error3');
         res.status(500).json(err);
@@ -67,7 +75,13 @@ router.post('/login', (req, res) => {
             return;
         }
 
-        res.json({user: dbUserData, message: 'You are now logged in!'});
+        req.session.save(() => {
+            req.session.user_id = dbUserData.id;
+            req.session.username = dbUserData.username;
+            req.session.loggedIn = true;
+
+          res.json({user: dbUserData, message: 'You are now logged in!'});  
+        });
     });
 });
 
